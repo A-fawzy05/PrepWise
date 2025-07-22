bcb3bc5ab82d6e80d9480631ec0b805377949c30
Amr ahmed <amrbusiness05@gmail.com>
2025-07-22 08:44:02 +0300

    test5

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true, // Helps with static file routing
-  images: { unoptimized: true }, // Required for static exports
   eslint: {
     ignoreDuringBuilds: true,
   },
