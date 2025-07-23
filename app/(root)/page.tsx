3de4b0eeeb548dc2af2a41dccdb09c9e71103c3d
Amr ahmed <amrbusiness05@gmail.com>
2025-07-23 07:50:09 +0300

    5las

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,19 +18,6 @@ function Page() {
     </div>
     <Image src="/robot.png" alt="robo-dude" width={400} height={400} className='max-sm:hidden'/>
   </section>
-  <section className='flex flex-col gap-6 mt-8'>
-    <h2>Your interviews</h2>
-    <div className='interviews-section'>
-      {dummyInterviews.map((interview) => (<InterviewCard {...interview} key={interview.id}/>))}
-    </div>
-  </section>
-  <section className='flex flex-col gap-6 mt-8'>
-    <h2>Take an interview</h2>
-    <div className='interviews-section'>
-      {dummyInterviews.map((interview) => (<InterviewCard {...interview} key={interview.id}/>))}
-      <p>There are no interviews available</p>
-    </div>
-  </section>
   </> );
 }
 
